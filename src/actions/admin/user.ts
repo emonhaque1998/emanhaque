d811a952fc1fe915021aa4a032ce7756655edaba
emonhaque1998 <emonhaque.net@gmail.com>
2025-05-27 05:20:18 +0600

    update

--- src/actions/admin/user.ts
+++ src/actions/admin/user.ts
@@ -8,42 +8,11 @@ export async function createUser() {
     return null; // User is not authenticated
   }
 
-  interface User {
-    emailAddresses?: { emailAddress: string }[];
-    firstName?: string;
-    lastName?: string;
-    publicMetadata?: {
-      role?: string;
-    };
-  }
+  const user = await prisma.user.findUnique({
+    where: { clerkId: userId },
+  });
 
-  const user = (await currentUser()) as User;
-  if (!user || !user.emailAddresses?.[0]?.emailAddress) {
-    return;
-  }
-
-  const email = user.emailAddresses[0].emailAddress;
-  const name = `${user.firstName || ""} ${user.lastName || ""}`.trim();
-  const role = user.publicMetadata?.role; // Default to 'user' if no role is set
-
-  try {
-    // Sync the user to your database
-    const createU = await prisma.user.upsert({
-      where: { clerkId: userId },
-      update: {},
-      create: {
-        clerkId: userId,
-        email,
-        name,
-        role,
-      },
-    });
-
-    return createU;
-  } catch (error) {
-    console.error("Error creating user:", error);
-    return null;
-  }
+  return user;
 }
 
 export async function getUser() {
