c160b936d77acfc3318810861951147015602f8a
emonhaque1998 <emonhaque.net@gmail.com>
2025-05-27 00:58:23 +0600

    udpate

--- src/components/server/SideBar.tsx
+++ src/components/server/SideBar.tsx
@@ -2,7 +2,5 @@ import { createUser } from "@/actions/admin/user";
 import MySelf from "../MySelf";
 
 export default async function SideBar() {
-  const user = await createUser();
-
-  return user ? <MySelf userInfo={user} /> : <MySelf />;
+  return <MySelf />;
 }
