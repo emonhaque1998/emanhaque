f62e67e10210ea888129e50d377a10fbe35a1539
emonhaque1998 <emonhaque.net@gmail.com>
2025-05-27 00:54:49 +0600

    udpate

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,3 @@
-import Banner from "@/components/Banner";
-import Footer from "@/components/Footer";
-import Header from "@/components/Header";
-import MyService from "@/components/Home/MyService";
-import MyStory from "@/components/Home/MyStory";
-import PricePlan from "@/components/Home/PricePlan";
-import Publications from "@/components/Home/Publications";
-import SkillOverview from "@/components/Home/SkillOverview";
-import Testimonials from "@/components/Home/Testimonials";
-import VideoResume from "@/components/Home/VideoResume";
-import HorizontalRowDotted from "@/components/HorizontalRowDotted";
-import LeftSideBar from "@/components/server/SideBar";
-
 export default function Home() {
-  return <></>;
+  return null; // or <div>Loading...</div>
 }
