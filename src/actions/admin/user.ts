2787da8468d08cdb6f4ed103a19833aa64fcc773
emonhaque1998 <emonhaque.net@gmail.com>
2025-05-26 15:41:35 +0600

    update

--- src/actions/admin/user.ts
+++ src/actions/admin/user.ts
@@ -20,7 +20,9 @@ export async function createUser() {
   }
 
   const user = (await currentUser()) as User;
-  if (!user || !user.emailAddresses?.[0]?.emailAddress) return;
+  if (!user || !user.emailAddresses?.[0]?.emailAddress) {
+    return;
+  }
 
   const email = user.emailAddresses[0].emailAddress;
   const name = `${user.firstName || ""} ${user.lastName || ""}`.trim();
