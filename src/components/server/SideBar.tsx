1d2176ca66ba78dc3141c2ad36e9eacb2bff6646
emonhaque1998 <emonhaque.net@gmail.com>
2025-05-27 13:44:40 +0600

    update

--- src/components/server/SideBar.tsx
+++
@@ -1,7 +0,0 @@
-import { createUser } from "../../app/actions/user";
-
-export default async function SideBar() {
-  const user = await createUser();
-
-  return <h1 className="text-lg font-bold">{user && user?.name}</h1>;
-}
