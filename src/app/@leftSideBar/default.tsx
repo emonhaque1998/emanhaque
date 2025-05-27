a8b715ff2a95badd8d292d30cf4d88efe2ded09e
emonhaque1998 <emonhaque.net@gmail.com>
2025-05-27 13:41:20 +0600

    update

--- src/app/@leftSideBar/default.tsx
+++ src/app/@leftSideBar/default.tsx
@@ -1,9 +1,11 @@
-import { createUser, getUser } from "@/app/actions/user";
+import { createUser, getUser } from "@/actions/user";
 import MySelf from "@/components/MySelf";
 import prisma from "@/lib/prisma";
+import { auth, clerkClient, currentUser } from "@clerk/nextjs/server";
 
-export default async function Default() {
-  const user = await createUser();
+export default async function LeftSideBar() {
+  const { userId } = await auth();
+  const user = await createUser(userId);
 
   return (
     <>
