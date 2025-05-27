dc3047caf0a66e4955dfb37509ecfde7ed1b1ca2
emonhaque1998 <emonhaque.net@gmail.com>
2025-05-27 12:22:45 +0600

    update

--- src/app/actions/user.ts
+++ src/app/actions/user.ts
@@ -4,9 +4,28 @@ import { auth, clerkClient } from "@clerk/nextjs/server";
 import prisma from "@/lib/prisma";
 
 export const createUser = async () => {
-  const { userId } = await auth();
+  // const { userId } = await auth();
 
-  return userId;
+  // if (!userId) return null;
+
+  const existingUser = await prisma.user.findFirst();
+
+  // if (existingUser) return existingUser;
+
+  // const client = await clerkClient();
+  // const clerkUser = await client.users.getUser(userId);
+  // const role = (clerkUser.publicMetadata.role as string) || "USER";
+
+  // const newUser = await prisma.user.create({
+  //   data: {
+  //     clerkId: userId,
+  //     email: clerkUser.emailAddresses[0].emailAddress,
+  //     name: `${clerkUser.firstName} ${clerkUser.lastName}`.trim(),
+  //     role: role,
+  //   },
+  // });
+
+  return existingUser;
 };
 
 export async function getUser() {
