870b285bdc7099ad3c063fb5aa2cc5d9084c3998
emonhaque1998 <emonhaque.net@gmail.com>
2025-05-31 11:53:15 +0600

    update

--- src/app/@mainContent/blog/[slug]/page.tsx
+++ src/app/@mainContent/blog/[slug]/page.tsx
@@ -1,5 +1,11 @@
-export default function SinglePost({ params }: { params: { slug: string } }) {
-  const slug = params.slug;
+import { use } from "react";
+
+export default function SinglePost({
+  params,
+}: {
+  params: Promise<{ slug: string }>;
+}) {
+  const { slug } = use(params);
   return (
     <>
       <h1>{slug}</h1>
