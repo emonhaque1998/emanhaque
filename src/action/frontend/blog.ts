2eb6d6955636e062b276d3533595b313c52b9702
emonhaque1998 <emonhaque.net@gmail.com>
2025-05-31 13:58:17 +0600

    update

---
+++ src/action/frontend/blog.ts
@@ -0,0 +1,5 @@
+export const getSinglePost = async (slug: string) => {
+  if (!slug) {
+    return { error: "Slug Not found" };
+  }
+};
--- src/app/@mainContent/blog/[slug]/page.tsx
+++ src/app/@mainContent/blog/[slug]/page.tsx
@@ -1,4 +1,11 @@
-import { use } from "react";
+"use client";
+import MainContainer from "@/components/MainContainer";
+import TopDivision from "@/components/TopDivision";
+import { use, useEffect } from "react";
+import { SlCalender } from "react-icons/sl";
+import { FaRegClock } from "react-icons/fa";
+import { RxAvatar } from "react-icons/rx";
+import { getSinglePost } from "@/action/frontend/blog";
 
 export default function SinglePost({
   params,
@@ -6,9 +13,48 @@ export default function SinglePost({
   params: Promise<{ slug: string }>;
 }) {
   const { slug } = use(params);
+
+  useEffect(() => {
+    const post = async () => {
+      await getSinglePost(slug);
+    };
+
+    post();
+  }, []);
   return (
     <>
-      <h1>{slug}</h1>
+      <TopDivision>
+        <MainContainer>
+          <div className="flex flex-col justify-center items-center w-full gap-10">
+            <div>
+              <SlCalender className="text-2xl" />
+            </div>
+            <div>
+              <h2 className="text-sm font-medium">September 24, 2020</h2>
+            </div>
+          </div>
+        </MainContainer>
+        <MainContainer>
+          <div className="flex flex-col justify-center items-center w-full gap-10">
+            <div>
+              <FaRegClock className="text-2xl" />
+            </div>
+            <div>
+              <h2 className="text-sm font-medium">September 24, 2020</h2>
+            </div>
+          </div>
+        </MainContainer>
+        <MainContainer>
+          <div className="flex flex-col justify-center items-center w-full gap-10">
+            <div>
+              <RxAvatar className="text-2xl" />
+            </div>
+            <div>
+              <h2 className="text-sm font-medium">September 24, 2020</h2>
+            </div>
+          </div>
+        </MainContainer>
+      </TopDivision>
     </>
   );
 }
