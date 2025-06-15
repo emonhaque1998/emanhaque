11b023b9a909cb83105d013a936f417716aff6dd
emonhaque1998 <emonhaque.net@gmail.com>
2025-06-15 11:08:19 +0600

    update

--- src/app/@mainContent/page.tsx
+++ src/app/@mainContent/page.tsx
@@ -18,7 +18,7 @@ export default function MainContent() {
       <MyService />
       <PricePlan />
       <Testimonials />
-      <Publications take={2} />
+      <Publications />
       <HorizontalRowDotted />
       <Footer />
     </>
