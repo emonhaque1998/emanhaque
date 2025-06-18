c6cbf2435faca13c78490c7059b6566939f1441b
emonhaque1998 <emonhaque.net@gmail.com>
2025-06-18 14:00:23 +0600

    update

--- src/components/Home/Publications.tsx
+++ src/components/Home/Publications.tsx
@@ -31,15 +31,15 @@ export default function Publications() {
   const [meta, setMeta] = useState<Meta | null>(null);
   const [loading, setLoading] = useState(true);
 
-  const fetchPosts = async () => {
-    setLoading(true);
-    const res = await axios.get(`/api/post?page=${page}&limit=${limit}`);
-    dispatch(addAllPost(res.data.posts));
-    setMeta(res.data.meta);
-    setLoading(false);
-  };
-
   useEffect(() => {
+    const fetchPosts = async () => {
+      setLoading(true);
+      const res = await axios.get(`/api/post?page=${page}&limit=${limit}`);
+      dispatch(addAllPost(res.data.posts));
+      setMeta(res.data.meta);
+      setLoading(false);
+    };
+
     fetchPosts();
   }, [page]);
 
