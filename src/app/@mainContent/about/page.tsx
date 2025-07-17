5928437cfb1788ac4ac8b219162483b3f3c3b556
emonhaque1998 <emonhaque.net@gmail.com>
2025-07-17 21:42:38 +0600

    update

--- src/app/@mainContent/about/page.tsx
+++ src/app/@mainContent/about/page.tsx
@@ -1,9 +1,13 @@
 import LanguageSkill from "@/components/About/LanguageSkill";
+import HorizontalRowDotted from "@/components/HorizontalRowDotted";
+import Footer from "@/components/Footer";
 
 export default function About() {
   return (
     <>
       <LanguageSkill />
+      <HorizontalRowDotted />
+      <Footer />
     </>
   );
 }
--- src/components/Portfolio/PortfolioComponent.tsx
+++ src/components/Portfolio/PortfolioComponent.tsx
@@ -1,26 +1,29 @@
 "use client";
 import axios from "axios";
 import PortfolioTitle from "./PortfolioTitle";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { useAppSelector, useAppDispatch } from "@/store/hooks";
 import { addAllCategory } from "@/store/categorySlice";
 import { addAllPortfolio } from "@/store/portfolioSlice";
 import Link from "next/link";
+import Loading from "../Loading";
 
 export default function PortfolioComponent() {
   const categoris = useAppSelector((state) => state.categorySlice.data);
   const portfolios = useAppSelector((state) => state.portfolioSlice.data);
   const dispatch = useAppDispatch();
+  const [loading, setLoading] = useState(false);
 
   useEffect(() => {
+    setLoading(true);
     const getPortfolio = async () => {
       const portfolioRes = await axios.get("/api/portfolio?take=10");
-
       const categoryRes = await axios.get(
         "/api/post/category/have-portfolio-category"
       );
       dispatch(addAllPortfolio(portfolioRes.data));
       dispatch(addAllCategory(categoryRes.data));
+      setLoading(false);
     };
 
     getPortfolio();
@@ -29,24 +32,28 @@ export default function PortfolioComponent() {
     <>
       <div className="flex flex-row w-full justify-between gap-3 max-md:flex-col max-md:-mt-20">
         <div className="bg-[#fcfcfe] rounded-lg shadow-lg dark:bg-[#00283a] py-10 px-10 flex-1 flex-col justify-center flex items-center">
-          <div className="w-full">
-            <div className="flex flex-row items-center gap-3 justify-start w-full">
-              {categoris &&
-                categoris.map((category, index) => (
-                  <Link
-                    href={`/portfolio/${category.categorySlug}`}
-                    key={index}
-                    className="bg-[#f4f5f7] text-sm dark:bg-[#02162b] px-5 cursor-pointer py-2 rounded-2xl hover:bg-[#70ba65] dark:hover:bg-[#70ba65] transition-all duration-300 text-[#00283A] dark:text-white font-semibold"
-                  >
-                    {category.categoryName}
-                  </Link>
-                ))}
+          {loading ? (
+            <Loading />
+          ) : (
+            <div className="w-full">
+              <div className="flex flex-row items-center gap-3 justify-start w-full">
+                {categoris &&
+                  categoris.map((category, index) => (
+                    <Link
+                      href={`/portfolio/${category.categorySlug}`}
+                      key={index}
+                      className="bg-[#f4f5f7] text-sm dark:bg-[#02162b] px-5 cursor-pointer py-2 rounded-2xl hover:bg-[#70ba65] dark:hover:bg-[#70ba65] transition-all duration-300 text-[#00283A] dark:text-white font-semibold"
+                    >
+                      {category.categoryName}
+                    </Link>
+                  ))}
+              </div>
             </div>
-          </div>
+          )}
         </div>
       </div>
       <div className="mt-5 grid grid-cols-2 gap-3 max-md:grid-cols-1">
-        <PortfolioTitle portfolios={portfolios} />
+        {loading ? <Loading /> : <PortfolioTitle portfolios={portfolios} />}
       </div>
     </>
   );
