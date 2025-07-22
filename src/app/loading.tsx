361c95b80a1f986d684a73d977381893a078522f
emonhaque1998 <emonhaque.net@gmail.com>
2025-07-23 02:12:15 +0600

    update

--- src/app/loading.tsx
+++ src/app/loading.tsx
@@ -1,8 +1,8 @@
 export default function Loading() {
   // You can add any UI inside Loading, including a Skeleton.
   return (
-    <div className="fixed inset-0 z-50 flex items-center justify-center backdrop-blur-sm bg-black/30">
-      <div className="text-white text-xl font-semibold">Loading...</div>
+    <div className="fixed inset-0 z-50 flex items-center justify-center bg-black/50 backdrop-blur-sm">
+      <div className="w-16 h-16 border-8 border-white border-rounded border-t-transparent rounded-full animate-spin"></div>
     </div>
   );
 }
